a78055827572d135854e34ea3a3e584b78d8f67a
Tilherme <gui.mauricio@hotmail.com.br>
2022-06-22 01:17:33 -0300

    integrçao graphgl

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+const GET_LESSONS_QUERY = gql `
+
+query{
+  lessons {
+    title
+    id
+  }
+}
+`
+interface Lesson {
+  id: string,
+  title: string
+}
 
 function App() {
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
+  console.log(data)
 
   return (
-    <h1 className="text-2xl text-violet-400 font-bold">ola</h1>
+   <ul>
+    {data?.lessons.map(lesson =>{
+      return <li key={lesson.id}>{lesson.title}</li>
+    })}
+   </ul>
   )
 }
 
